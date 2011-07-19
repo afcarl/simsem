53c0ec0beb39daeed04d663b9941053477bbba88
Pontus Stenetorp <pontus@stenetorp.se>
2011-07-19 15:54:31 +0900

    New chained build.sh.

--- build.sh
+++ build.sh
@@ -7,14 +7,14 @@
 ARCH_DIR='external'
 
 LIBLINEAR='liblinear-1.7'
-( cd ${ARCH_DIR} && rm -rf ${LIBLINEAR} && tar xfz ${LIBLINEAR}.tar.gz && \
-    cd ${LIBLINEAR} && make && cd python && make )
-
 SIMSTRING='simstring-1.0'
-( cd ${ARCH_DIR} && rm -rf ${SIMSTRING} && tar xfz ${SIMSTRING}.tar.gz && \
-    cd ${SIMSTRING} && ./configure && cd swig/python && ./prepare.sh && \
-    python setup.py build_ext --inplace )
 
-LIBSVM='libsvm-3.1'
-( cd ${ARCH_DIR} && rm -rf ${LIBSVG} && tar xfz ${LIBSVM}.tar.gz && \
-    cd ${LIBSVM} && make && cd python && make )
+(
+cd ${ARCH_DIR} && rm -rf ${LIBLINEAR} && tar xfz ${LIBLINEAR}.tar.gz && \
+    cd ${LIBLINEAR} && make && cd python && make
+    ) && (
+
+cd ${ARCH_DIR} && rm -rf ${SIMSTRING} && tar xfz ${SIMSTRING}.tar.gz && \
+    cd ${SIMSTRING} && ./configure && cd swig/python && ./prepare.sh && \
+    python setup.py build_ext --inplace
+)
