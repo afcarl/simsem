8070ef8615e38d480037a97fcc0a395c2e944e0a
Pontus Stenetorp <pontus@stenetorp.se>
2011-07-29 10:52:01 +0900

    Preparation script now also generates features.

--- prepare.sh
+++ prepare.sh
@@ -8,8 +8,10 @@
 (
 # Download external resources and make the databases
 cd data/simstring/res && gmake ext_res && gmake
-) &&
-(
+) && (
 # Build external resources
 ./build.sh
+) && (
+# Generate dynamic features from the databases
+classifier/simstring/generate.py features > classifier/simstring/features.py
 )
