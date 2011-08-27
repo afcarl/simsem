321acea58ef9a6582fb27e5c33cabb82ae6e4193
Pontus Stenetorp <pontus@stenetorp.se>
2011-08-27 17:25:49 +0900

    Guard against xargs bug.

--- prepare.sh
+++ prepare.sh
@@ -27,10 +27,10 @@ classifier/simstring/generate.py features > classifier/simstring/features.py
 ) && (
 # Extract corpora resources
 find data/corpora/ -name '*.tar.gz' \
-    | xargs -r -I {} sh -c 'tar -x -z -f {} -C `dirname {}`'
+    | xargs -r -i{} sh -c 'tar -x -z -f {} -C `dirname {}`'
 ) && (
 find data/corpora/ -name '*.zip' \
-    | xargs -r -I {} sh -c 'unzip -o -d `dirname {}` {}'
+    | xargs -r -i{} sh -c 'unzip -o -d `dirname {}` {}'
 ) && (
 # Correct offset error in the GREC data set
 sed -i -e 's|\(T4\tGene \)423\(.*\)|\1422\2|g' \
@@ -38,5 +38,5 @@ sed -i -e 's|\(T4\tGene \)423\(.*\)|\1422\2|g' \
 ) && (
 # The GREC sentence split files have non-conforming filenames, correct them
 find data/corpora/grec -name '*.txt.ss' | sed -e 's|\.txt\.ss||g' \
-    | xargs -I {} sh -c 'mv {}.txt.ss {}.ss'
+    | xargs -i{} sh -c 'mv {}.txt.ss {}.ss'
 )
