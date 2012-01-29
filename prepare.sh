214a7a7ba8474d493e188b82ded8e5754e1c6aa7
Pontus Stenetorp <pontus@stenetorp.se>
2012-01-29 14:25:17 +0900

    Removed now redundant corpus pre-processing.

--- prepare.sh
+++ prepare.sh
@@ -24,23 +24,4 @@ cd data/simstring/res && make_cmd ext_res && make_cmd
 # NOTE: This is one of the ugliest mistakes in design I have made over the
 # last few years, shame on me
 classifier/simstring/generate.py features > classifier/simstring/features.py
-) && (
-# Extract corpora resources
-find data/corpora/ -name '*.tar.gz' \
-    | xargs -r -i{} sh -c 'tar -x -z -f {} -C `dirname {}`'
-) && (
-find data/corpora/ -name '*.zip' \
-    | xargs -r -i{} sh -c 'unzip -o -d `dirname {}` {}'
-) && (
-# Correct offset error in the GREC data set
-sed -i -e 's|\(T4\tGene \)423\(.*\)|\1422\2|g' \
-    data/corpora/grec/GREC_Standoff/Human/8205615.a1
-) && (
-# The GREC sentence split files have non-conforming filenames, correct them
-find data/corpora/grec -name '*.txt.ss' | sed -e 's|\.txt\.ss||g' \
-    | xargs -i{} sh -c 'mv {}.txt.ss {}.ss'
-) && (
-# GREC uses DOS newlines, kill them with fire
-find data/corpora/grec -name '*.txt' -o -name '*.a1' -o -name '*.a2' \
-    -o -name '*.ss' -o -name '*.mcccj' | xargs -r dos2unix
 )
